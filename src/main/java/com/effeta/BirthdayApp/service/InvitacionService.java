0ced516ebdffd3a43259eeb44632f4998848c380
Lattes_and_Cream <santiago.guerra@mail.escuelaing.edu.co>
2026-02-05 19:53:21 -0500

    Más invitados

--- src/main/java/com/effeta/BirthdayApp/service/InvitacionService.java
+++ src/main/java/com/effeta/BirthdayApp/service/InvitacionService.java
@@ -23,12 +23,16 @@ public class InvitacionService {
         crearInvitado(new Invitado("8", "Camila Rodriguez", "+573022991078", "fiesta1", false));
         crearInvitado(new Invitado("9", "Mapis Peñuela", "+573213880189", "fiesta1", false));
         crearInvitado(new Invitado("10", "Brayan Jimenez", "+573144560694", "fiesta1", false));
-        crearInvitado(new Invitado("11", "Julian Parra", "+573127701620", "fiesta1", false));
-        crearInvitado(new Invitado("12", "Samu medina", "+5732125278525", "fiesta1", false));
+        crearInvitado(new Invitado("11", "Julian Parra", "+573006400368", "fiesta1", false));
+        crearInvitado(new Invitado("12", "Samu medina", "+573212527852", "fiesta1", false));
         crearInvitado(new Invitado("13", "Nelson Monroy", "+573107220130", "fiesta1", false));
         crearInvitado(new Invitado("14", "Richard Rodriguez", "+573177970213", "fiesta1", false));
         crearInvitado(new Invitado("15", "Vanesa Rincon", "+573193927132", "fiesta1", false));
         crearInvitado(new Invitado("16", "Cristian Rodriguez", "+573192713167", "fiesta1", false));
+        crearInvitado(new Invitado("17", "David Parra", "+573127701620", "fiesta1", false));
+        crearInvitado(new Invitado("18", "Sebastián Aguilar", "+573115967530", "fiesta1", false));
+        crearInvitado(new Invitado("19", "Manuel Aguilar", "+573115970103", "fiesta1", false));
+        crearInvitado(new Invitado("20", "Manuela Robayo", "+573164938000", "fiesta1", false));
     }
 
     public Invitado crearInvitado(Invitado invitado) {
